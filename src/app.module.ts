787c4be5b8f219ca6c3aaba1c9fa1269a51f9ee3
Mauricio Gonzalo Peredo Saracho <mauriciogonzaloperedo28@gmail.com>
2022-10-17 00:45:43 -0400

    configuracion para conexion a base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,34 @@
+import { Postt } from './entities/post.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsModule } from './posts/post.module';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [PostsModule]
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [Postt],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    TypeOrmModule.forFeature([Postt]),
+    PostsModule,
+    CommonModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
